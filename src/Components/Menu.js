8841257ad5439192d16cc9dfa878a7e49f2f1fbd
Jisung Kang <jisungk@berkeley.edu>
2022-10-24 11:10:45 -0700

    checking color and menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Color from './Color'
 /* Add any imports you think you might need here! */
 
 const Menu = () => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="red" />
+          <Color color="pink" />
+          <Color color="blue" />
+          <Color color="green" />
       </div>
     );
 }
