c87e5b988f0367a89f8e068a055cf8b952e6a7a3
Jisung Kang <jisungk@berkeley.edu>
2022-10-24 23:50:01 -0700

    checking block and menu.js

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,17 +2,33 @@ import { useState } from 'react';
 import React from 'react';
 import Menu from './Menu';
 import Block from './Block';
+import Color from './Color';
 
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [curr, setCurr] = useState([]);
+    const [color, changeColor] = useState("none")
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = curr.map((c) => {
+        return (
+            <div>
+                <Block color={c}/>
+            </div>
+        )
+    }
+    ); // TODO: edit this variable
+
+    function onClick(c){
+        setCurr(curr => [...curr, c]) 
+        console.log(curr)
+    }
 
     return (
         <div>
-            <Menu></Menu>
+            <Menu>
+            </Menu>
 
             {/* Below is where all of your Blocks should render! */}
             {posts}
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
-import Color from './Color'
+import React, {useState} from 'react';
+import Color from './Color';
+import Block from './Block';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+function Menu() {
+
+  const [color, changeColor] = useState("none")
+
+  const clicked = (c) => {
+    return (
+      <div>
+        <Block color={c}/>
+      </div>
+    )
+  }
 
     return (
       <div className="colorOptions">
-          <Color color="red" />
-          <Color color="pink" />
-          <Color color="blue" />
-          <Color color="green" />
+        <Color color="red" handleClick={() => changeColor("red")}/>
+        <Color color="pink" handleClick={() => changeColor("pink")}/>
+        <Color color="blue" handleClick={() => changeColor("blue")}/>
+        <Color color="green" handleClick={() => changeColor("green")}/>
+        {color == "red" && clicked("red")}
+        {color == "pink" && clicked("pink")}
+        {color == "blue" && clicked("blue")}
+        {color == "green" && clicked("green")}
       </div>
     );
 }
